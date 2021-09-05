f8306b167d4896cb5bfe80d3ef9e0cfdb0d9a7cb
ggroshansii <garthgroshansii@gmail.com>
2021-09-04 23:02:04 -0400

    completed 'Stop gninnipS My sdroW' 6kyu challenge

---
+++ ReverseSome_6Kyu.js
@@ -0,0 +1,32 @@
+/*
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the name of this kata).
+
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+
+*/
+
+function spinWords(string) {
+    let splitStr = string.split(" ");
+    let updatedStr = [];
+    for (let i = 0; i < splitStr.length; i++) {
+        if (splitStr[i].length < 5) {
+            if (updatedStr.length === 0) {
+                updatedStr.push(splitStr[i]);
+            } else {
+                updatedStr.push(" ", splitStr[i]);
+            }
+        } else {
+            let reversedWord = splitStr[i].split("").reverse().join("");
+            if (updatedStr.length === 0) {
+                updatedStr.push(reversedWord);
+            } else {
+                updatedStr.push(" ", reversedWord);
+            }
+        }
+    }
+    return updatedStr.join("");
+}
+
+console.log(spinWords("welcome"));
